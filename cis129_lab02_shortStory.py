9f66db49ff5e00c274b25b454d085abfa065d8c6
Marif2 <marif2@mail.pima.edu>
2024-09-26 14:42:10 -0700

    Update cis129_lab02_shortStory.py
    I added some print statements 09/26/24

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Nabi ARIF
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('Nabi ARIF')
+print( 'My first Python lab in my CIS129 course')
+print('Prints a simple short story')
+
